d443fa818f495b92900e34c39ad9d848240444f5
yomunnib <muneeb.cheema36@gmail.com>
2022-08-15 21:03:17 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (playerSelection == computerSelection) {
+    return ("Draw");
+ } else {
+    return ("not draw");
+}
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 
 /*
