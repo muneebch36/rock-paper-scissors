e4f5bea40db6c1be2743a41e1366da035388e688
yomunnib <muneeb.cheema36@gmail.com>
2022-08-16 04:49:05 -0400

    Adjust round logic

--- script.js
+++ script.js
@@ -4,38 +4,35 @@
 
 
 // function getComputerChoice() {
-// let computerSelection = choice[Math.floor(Math.random() * choice.length)];
+
 // return computerSelection;
 // }
 
 // function getPlayerChoice() {
-// let playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+// 
 // return playerSelection;
 // }
 let playerWin = 0;
 let computerWin = 0;
 
-function playRound() {
-  let choice = ["rock", "paper", "scissors"];
-  let computerSelection = choice[Math.floor(Math.random() * choice.length)];
-  let playerSelection = "rock";
+function playRound(playerSelection,computerSelection) {
 
 if (playerSelection == computerSelection) {
     return ("tie");
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return ++computerWin;
+    ++computerWin;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return ++playerWin;
+    ++playerWin;
 } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return ++computerWin;
+    ++computerWin;
 } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return ++playerWin;
+    ++playerWin;
 } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return ++computerWin;
+    ++computerWin;
 } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return ++playerWin;
+    ++playerWin;
   } else {
-    alert ("User input error");
+    return ("User input error");
   }
 }
 
@@ -51,7 +48,8 @@ console.log(computerWin);
 }
 
 
-// const computerSelection = getComputerChoice();
-// const playerSelection = getPlayerChoice();
+let playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+let choice = ["rock", "paper", "scissors"];
+let computerSelection = choice[Math.floor(Math.random() * choice.length)];
 // console.log(playRound(playerSelection, computerSelection));
 // console.log(game(playRound));
