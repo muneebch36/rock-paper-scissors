90737f5fcc413c9cb315364d857597c74651599a
yomunnib <muneeb.cheema36@gmail.com>
2022-08-16 00:24:17 -0400

    Adjust psuedocode and game function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let choice = ["rock", "paper", "scissors"];
 let playerWin = 0;
+let computerWin = 0;
+let draw = 0;
 let rounds = 0;
 
 let getComputerChoice = () => {
@@ -10,34 +12,41 @@ return computerSelection;
 
 function playRound(playerSelection, computerSelection) {
 if (playerSelection == computerSelection) {
-    return ("Draw");
+    return ++draw;
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return ("computer wins");
+    return ++computerWin;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return ++playerWin;
+    return ("playerWin")
 } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return ("computer wins");
+    return ++computerWin;
 } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return ++playerWin;
+    return ("playerWin")
 } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return ("computer wins");
+    return ++computerWin;
 } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return ++playerWin;
+    return ("playerWin")
   } else {
     return ("User input error");
   }
 }
 
-function game(playRound)
+function game(playRound) {
 for (let i = 0; i < 5; i++) {
-rounds++;
+++rounds;
+++computerWin;
+++playerWin;
+if (playerWin == 3) {
+  return ("player wins");
+ } else {
+  return ("computer wins");
+ }
+}
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
-console.log(playerWin);
-
+console.log(game(playRound));
 
 /*
 console.log(playRound(playerSelection, computerSelection));
